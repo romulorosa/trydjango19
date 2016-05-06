90e29c1a21a620eda0aa2b0feb58dd8c5fd9e535
Romulo Rosa Furtado <romuloros@gmail.com>
2016-05-06 20:39:03 -0300

    Testing post template

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+from posts.views import post_list
 
-# Create your tests here.
+class IndexPageViewTest(TestCase):
+    def test_home_page_user_posts_with_user(self):
+        request = HttpRequest()
+        context = {'user_name': 'romulo'}
+        response = post_list(request, context)
+        observed_content = response.content.decode('utf8')
+        expected_content = render_to_string('index.html', {'user_name':'romulo'})
+        self.assertEqual(observed_content, expected_content)
+
+    def test_home_page_user_posts_without_user(self):
+        request = HttpRequest()
+        context = {}
+        response = post_list(request, context)
+        observed_content = response.content.decode('utf8')
+        expected_content = render_to_string('index.html', {})
+        self.assertEqual(observed_content, expected_content)
