dd920bee27a59dc596d84bff19a5decc5faa9027
Romulo Rosa Furtado <romuloros@gmail.com>
2016-05-06 20:41:03 -0300

    Setting default value of context on post_list

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def post_list(request, context):
+def post_list(request, context={}):
     #return HttpResponse("<h1>LIST</h1>")
     return render(request,"index.html", context)
 
