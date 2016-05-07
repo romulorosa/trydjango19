a586bd948787ec3d65f1d7cb9a7a86622b32fc57
Romulo Rosa Furtado <romuloros@gmail.com>
2016-05-07 10:11:06 -0300

    Refactoring link to post detail using url name

--- posts/urls.py
+++ posts/urls.py
@@ -10,7 +10,7 @@ from .views import (
 
 urlpatterns = [
     url(r'^create/$', post_create),
-    url(r'^detail/(?P<post_id>\d+)/$', post_detail),
+    url(r'^detail/(?P<post_id>\d+)/$', post_detail, name="detail"),
     url(r'^update/$', post_update),
     url(r'^delete/$', post_delete),
     url(r'^$', post_list),
