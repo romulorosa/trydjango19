ec63f232d21f708634754eed4a87f0568847c5b2
Romulo Rosa Furtado <romuloros@gmail.com>
2016-05-07 10:06:16 -0300

    Include post id on url to get details

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
 
 from .models import Post
 # Create your views here.
 def post_list(request, context={}):
     #return HttpResponse("<h1>LIST</h1>")
+    object_list = get_list_or_404(Post)
+    context = {
+        "title": "Post list",
+        "object_list": object_list,
+    }
     return render(request,"index.html", context)
 
 def post_delete(request):
@@ -16,7 +21,7 @@ def post_update(request):
 def post_create(request):
     return HttpResponse("<h1>CREATE</h1>")
 
-def post_detail(request, post_id):
+def post_detail(request, post_id=1):
     #instance = Post.objects.get(id=1)
     instance = get_object_or_404(Post, id=post_id)
     context = {
