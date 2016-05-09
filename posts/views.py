ef46a6f146a1dadb6a46162daadaac0521e9414a
Romulo Rosa Furtado <romuloros@gmail.com>
2016-05-08 21:57:08 -0300

    Post create request

--- posts/views.py
+++ posts/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
 
 from .models import Post
-# Create your views here.
-def post_list(request, context={}):
+from .forms import PostForm
+
+def post_list(request):
     #return HttpResponse("<h1>LIST</h1>")
     object_list = get_list_or_404(Post)
     context = {
@@ -18,8 +19,8 @@ def post_delete(request):
 def post_update(request):
     return HttpResponse("<h1>UPDATE</h1>")
 
-def post_create(request):
-    return HttpResponse("<h1>CREATE</h1>")
+# def post_create(request):
+#     return HttpResponse("<h1>CREATE</h1>")
 
 def post_detail(request, post_id=1):
     #instance = Post.objects.get(id=1)
@@ -29,3 +30,15 @@ def post_detail(request, post_id=1):
         "instance": instance,
     }
     return render(request,"post_detail.html", context)
+
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+
+    context = {
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
